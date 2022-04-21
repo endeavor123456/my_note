82b4a262f2cd5ca5d8d6a2707ca1f8468fd2cf86
wangyanlin <wangyanlin330231@163.com>
2022-04-21 13:47:42 +0800

    alter func init

--- func/init函数.go
+++ func/init函数.go
@@ -25,6 +25,14 @@ init 函数通常被用来：
 	4)运行一次计算
 二、Go要求非常严格，不允许引用不使用的包。但是有时你引用包只是为了调用init函数去做一些初始化工作。此时空标识符（也就是下划线）的作用就是为了解决这个问题
  _ "github.com/go-sql-driver/mysql"
+三、init函数只能被系统调用 也不可赋值给函数变量 否则会报错
+四、导入包不要出现循环依赖，否则会导致报错
+五、init不应该依赖任何在main函数里定义的变量，因为init函数的执行顺序是在main函数之前的
+六、init函数中也可以启动goroutine，也就是在初始化的同时启动新的goroutine，这并不会影响初始化顺序
+七、复杂逻辑不建议使用init函数，会增加代码的复杂性，可读性会下降
+八、不要依赖init函数执行的顺序
+九、无论包被导入多少次，init函数只能被调用一次，也就是执行一次
+_ "github.com/go-sql-driver/mysql"和"github.com/go-sql-driver/mysql"mysql包被导入了两次 而只里面的init函数只会执行一次 init函数不会因为其所在的包被导入两次就执行两次
 */
 func main() {
 	fmt.Println("main()")
